e748e53dda995a99a2e44d3f2cd9790fdc9aab9e
Bwslea <154704004+Bwslea@users.noreply.github.com>
2024-05-12 17:07:46 +0200

    Add files via upload

---
+++ MyProjectPHP/Index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Sur les traces de Vauban </title>
+    <link rel="stylesheet" href="Css/IndexStyle.css">
+</head>
+<body>
+<div id="Block">
+    <header>
+        <div class="main_title">
+            <img src="./Images/bitmoji.png" height="110" alt="my_caricature ">
+            <div>
+            <h1>Brouwers Léa</h1>
+            <h2>My Project</h2>
+            </div>
+        </div>
+        <nav class="navigation_menu">
+            <ul>
+                <li class="deroulement"><a href="#">Ma Haute Ecole Condorcet</a>
+                    <div class="onglets">
+                        <a href="./Pages/ReportagePref.html">Heph Condorcet</a>
+                        <a href="./Pages/Reportages.html#VdC">Section e-business</a>
+                    </div>
+                </li>
+                <li class="deroulement"><a href="./Pages/CalculsAffectations.html">Calculs et affectations</a>
+                    <div class="onglets">
+                        <a href="./CalculsAffectations.html#Rect">Rectangle</a>
+                        <a href="./CalculsAffectations.html#Tv">TVA</a>
+                    </div>
+                </li>
+                <li class="deroulement"><a href="./Pages/StructuresConditionnelles.html">Structures conditionnelles</a>
+                    <div class="onglets">
+                        <a href="./Pag">Cercle</a>
+                        <a href="./Page">Fonction quadratique</a>
+                        <a href="./Page">Calculette</a>
+                        <a href="./Page">Civilité</a>
+                        <a href="./Page">Date Longue</a>
+                    </div>
+                </li>
+                <li class="deroulement"><a href="./Pages/StructuresRépétitivesEtFonctions.html">Structures répétitives et fonctions</a></li>
+                <li class="deroulement"><a href="./Pages/Tableaux.html">Tableaux</a></li>
+                <li class="deroulement"><a href="./Pages/FinApprentissage.html">Fin d’apprentissage </a>
+                    <div class="onglets">
+                        <a href="./Pages/FinApprentissage.html#Tr">Tris</a>
+                    </div>
+                </li>
+            </ul>
+        </nav>
+        </div>
+    </header>
+    <div class="banner">
+        <h2 class="titreImage"> Retour de mes escapades avec VAUBAN</h2>
+        <div class="description_banner"></div>   
+    
+    <section>
+    </section>
+
+    <footer>
+    </footer>
+</div>
+</body>
+</html>
---
+++ MyProjectPHP/Pages/FinApprentissage.php
@@ -0,0 +1,61 @@
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../Css/IndexStyle.css">
+    <link rel="stylesheet" href="../Css/Projets.css">
+    <title>My Project</title>
+</head>
+<body>
+    <header>
+        <div class="main_title">
+            <img src="../Images/bitmoji.png" height="110" alt="my_caricature ">
+            <div>
+            <h1>Brouwers Léa</h1>
+            <h2>My Project</h2>
+            </div>
+        </div>
+        <nav class="navigation_menu">
+            <ul>
+                <li><a href="../Index.html">But de l’unité programmation</a></li>
+                <li class="deroulement"><a href="./HauteEcoleCondorcet.html">Haute Ecole Condorcet</a>
+                    <div class="onglets">
+                        <a href="./HauteEcoleCondorcet.html#HepCond">Heph Condorcet</a>
+                        <a href="./HauteEcoleCondorcet.html#SecEbus">Section e-business</a>
+                    </div>
+                </li>
+                <li class="deroulement"><a href="#">Calculs et affectations</a>
+                    <div class="onglets">
+                        <a href="./Rectangle.html">Rectangle</a>
+                        <a href="./Tva.html">TVA</a>
+                    </div>
+                </li>
+                <li class="deroulement"><a href="./StructuresConditionnelles.html">Structures conditionnelles</a>
+                    <div class="onglets">
+                        <a href="./StructuresConditionnelles.html#"Cerc>Cercle</a>
+                        <a href="./StructuresConditionnelles.html#FonctQuad">Fonction quadratique</a>
+                        <a href="./StructuresConditionnelles.html#Calc">Calculette</a>
+                        <a href="./StructuresConditionnelles.html#Civi">Civilité</a>
+                        <a href="./StructuresConditionnelles.html#DaLong">Date Longue</a>
+                    </div>
+                </li>
+                <li><a href="./StructuresRépétitivesEtFonctions.html">Structures répétitives et fonctions</a></li>
+                <li><a href="./Tableaux.html">Tableaux</a></li>
+                <li class="deroulement"><a href="#">Fin d’apprentissage </a>
+                    <div class="onglets">
+                        <a href="#Tr">Tris</a>
+                    </div>
+                </li>
+            </ul>
+        </nav>
+    </header>
+    <section id="Aldrin">
+        <h1>Projet Aldrin</h1>
+        <p></p>
+        <p></p> <br>
+        <hr> <br>
+        <form class="formulaire">
+        </form>
+    </section>
+</body>
+</html>
